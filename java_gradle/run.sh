597cd0af27d6b01710f122eb79d2caa492d21a70
Haroon Sheikh <haroon.sheikh@outlook.com>
2023-07-25 17:32:36 +0100

    Updates to use java 11 and latest dependencies (#1)

--- java_gradle/run.sh
+++ java_gradle/run.sh
@@ -14,4 +14,4 @@ out_file="$BENCHMARK_OUT_DIR/java_gradle_$run.csv"
 printf "%s," $REVISION >> $out_file
 
 ./gradlew clean > /dev/null 2>&1
-/usr/bin/time -f "%P,%M,%E,%x" -a -o $out_file ./gradlew --no-daemon --no-build-cache gauge -PinParallel=true > /dev/null 2>&1
+/usr/bin/time -f "%P,%M,%E,%x" -a -o $out_file ./gradlew --no-daemon --no-build-cache gauge $flags > /dev/null 2>&1
